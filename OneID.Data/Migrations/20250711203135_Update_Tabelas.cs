d39b67fa83aa607fa78c413a81df6a6b0b9d9e0c
Rubens Fiorelli <rubensfiorelli@outlook.com>
2025-07-11 17:32:28 -0300

    Add-Migrations

---
+++ OneID.Data/Migrations/20250711203135_Update_Tabelas.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OneID.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update_Tabelas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateOfHire",
+                table: "tb_oneid_user_accounts",
+                newName: "StartDate");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Department",
+                table: "tb_oneid_user_accounts",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Department",
+                table: "tb_oneid_user_accounts");
+
+            migrationBuilder.RenameColumn(
+                name: "StartDate",
+                table: "tb_oneid_user_accounts",
+                newName: "DateOfHire");
+        }
+    }
+}
