07dc2eef357f9d93f84dd06b59a8346df6f6274f
Rubens Fiorelli <rubensfiorelli@outlook.com>
2025-07-12 00:32:18 -0300

    update

---
+++ OneID.Domain/Entities/Packages/AccessPackage.cs
@@ -0,0 +1,57 @@
+﻿using OneID.Domain.Abstractions.EventsContext;
+using OneID.Domain.Contracts;
+using OneID.Domain.Notifications;
+#nullable disable
+namespace OneID.Domain.Entities.Packages
+{
+    public class AccessPackage : IAggregateRoot
+    {
+        private readonly List<AccessPackageItem> _items = [];
+        private readonly List<Notification> _notifications = [];
+        private readonly List<Event> _events = [];
+
+        public AccessPackage(string name, string createdBy)
+        {
+            Name = name;
+            CreatedBy = createdBy;
+            UpdatedBy = createdBy;
+            ProvisioningAt = DateTimeOffset.UtcNow;
+            _items = [];
+        }
+
+        public string Id { get; private set; }
+        public string Name { get; private set; }
+
+        public string CreatedBy { get; private set; }
+        public string UpdatedBy { get; private set; }
+        public DateTimeOffset ProvisioningAt { get; private set; }
+        public DateTimeOffset? UpdatedAt { get; private set; }
+
+        public IReadOnlyCollection<AccessPackageItem> Items => _items.AsReadOnly();
+        public IReadOnlyCollection<Notification> Notifications => _notifications.AsReadOnly();
+        public IReadOnlyCollection<Event> Events => _events.AsReadOnly();
+
+        public void AddItem(string type, string value)
+        {
+            var item = new AccessPackageItem(Id, type, value);
+            _items.Add(item);
+        }
+
+        public void SetUpdatedBy(string updatedBy)
+        {
+            UpdatedBy = updatedBy;
+            UpdatedAt = DateTimeOffset.UtcNow;
+        }
+
+        public void AddEvent(Event domainEvent) => _events.Add(domainEvent);
+        public void ClearEvents() => _events.Clear();
+
+        public void AddNotification(Notification notification) => _notifications.Add(notification);
+        public void AddNotifications(IEnumerable<Notification> notifications) => _notifications.AddRange(notifications);
+
+        public void PrepareForUpdate(string updatedBy) => SetUpdatedBy(updatedBy);
+
+        public bool IsValid() => true;
+    }
+
+}
---
+++ OneID.Domain/Entities/Packages/AccessPackageCondition.cs
@@ -0,0 +1,23 @@
+﻿#nullable disable
+namespace OneID.Domain.Entities.Packages
+{
+    public class AccessPackageCondition
+    {
+        public AccessPackageCondition(string accessPackageId, string department, string jobTitle)
+        {
+            AccessPackageId = accessPackageId;
+            Department = department;
+            JobTitle = jobTitle;
+        }
+
+        public string Id { get; private set; }
+        public string AccessPackageId { get; private set; }
+        public string Department { get; private set; }
+        public string JobTitle { get; private set; }
+        public string DepartmentId { get; private set; }
+        public string JobTitleId { get; private set; }
+
+        public AccessPackage AccessPackage { get; private set; }
+    }
+
+}
