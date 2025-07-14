403a99bd58df0dd6b67f52c42eb05f1fce32b657
Rubens Fiorelli <rubensfiorelli@outlook.com>
2025-07-14 12:28:06 -0300

    Add Home Controller

---
+++ OneID.Api/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OneID.Api.Controllers
+{
+    [Route("")]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class HomeController : ControllerBase
+    {
+
+        [HttpGet("")]
+        public IActionResult Home()
+        {
+            return Ok(CreateSystemInfoResponse());
+        }
+
+        [HttpGet("health", Name = "CheckHealth")]
+        public IActionResult HealthCheck()
+        {
+            return Ok(new { status = "Healthy" });
+        }
+        private static object CreateSystemInfoResponse()
+        {
+            return new { Environment.Is64BitOperatingSystem };
+        }
+
+    }
+}
