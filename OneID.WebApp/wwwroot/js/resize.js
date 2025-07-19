bcada532896d2e57736bc1e3ecd9b08ad7702169
Rubens Fiorelli <rubensfiorelli@outlook.com>
2025-07-19 19:44:16 -0300

    Update Dashboard...resizable

---
+++ OneID.WebApp/wwwroot/js/resize.js
@@ -0,0 +1,15 @@
+﻿window.enableResize = (id) => {
+    const el = document.getElementById(id);
+    if (!el) {
+        console.warn("Elemento não encontrado:", id);
+        return;
+    }
+
+    el.style.position = 'relative';
+    el.style.resize = 'both';
+    el.style.overflow = 'auto';
+    el.style.minWidth = '200px';
+    el.style.minHeight = '150px';
+    el.style.maxWidth = '100%';
+    el.style.maxHeight = '90vh';
+};
